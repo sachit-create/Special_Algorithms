740e88315f06294b6184b3ec8d0710e097b7d43f
sachit-create <sachitkohli147@gmail.com>
2024-08-08 19:33:20 +0530

    now you can use it

--- Special_Algorithms/bucketsort.cpp
+++ Special_Algorithms/bucketsort.cpp
@@ -1,14 +1,33 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
+#include <iomanip>
 using namespace std;
 
 void bucket_sort(float *arr , int n){
-   vector<int> v(n);
-  for(int i=0 ; i<n ; i++){
-      float idx = arr[i] * n;
-      v[idx] = arr[i];
-  }
+    vector<vector <float> > bucket(n , vector<float> ());
+
+     //step2: inserting elements into bucket.
+     for(int i=0 ; i<n ;i++){
+      int idx = arr[i]*n;
+      bucket[idx].push_back(arr[i]);
+     }
+
+     // step3: sorting individual buckets.
+     for(int i=0 ; i<n ; i++){
+        if(!bucket[i].empty()){
+          sort(bucket[i].begin() , bucket[i].end());
+        }
+     }
+     //step4: combining elements from buckets;
+     int k = 0;
+     for(int i=0 ; i<n ; i++){
+        for(int j=0 ; j<bucket[i].size() ; j++){
+          arr[k++] = bucket[i][j];
+        }
+     }
 }
+
 int main(){
     int n;
     cin >> n;
@@ -20,7 +39,7 @@ int main(){
     bucket_sort(arr,n);
 
     for(int i=0 ; i<n ; i++){
-        cout << arr[i] << " ";
+        cout <<fixed << setprecision(3) << arr[i] << " ";
     }
     return 0;
 }
